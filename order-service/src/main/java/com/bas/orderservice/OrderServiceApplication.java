b3a3650fd2d5144c54037d9dc19efa2c6b7033fa
Berat <beratkalhan99@gmail.com>
2023-12-09 05:31:52 -0800

    test

--- order-service/src/main/java/com/bas/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/bas/orderservice/OrderServiceApplication.java
@@ -8,6 +8,7 @@ public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+		System.out.println("test");
 	}
 
 }
