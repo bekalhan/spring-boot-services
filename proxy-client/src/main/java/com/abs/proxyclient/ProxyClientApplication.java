204d549bdac07361495d77c047e7c87406daa68b
Aytuğ Özkaya <aaytugozkaya@gmail.com>
2024-01-04 17:32:46 +0300

    Proxy-client structure added.

---
+++ proxy-client/src/main/java/com/abs/proxyclient/ProxyClientApplication.java
@@ -0,0 +1,13 @@
+package com.abs.proxyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyClientApplication.class, args);
+	}
+
+}
